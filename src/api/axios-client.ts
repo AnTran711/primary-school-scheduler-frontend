140590351e31d810ab1f4d8d70c736297a5c2c08
An Tran <tranquocan012017@gmail.com>
2026-06-21 21:27:14 +0700

    add env file

--- src/api/axios-client.ts
+++ src/api/axios-client.ts
@@ -3,8 +3,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1',
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
